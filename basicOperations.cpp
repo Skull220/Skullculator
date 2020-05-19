21731efc69e0df353c093aadf6c619653d87690f
Skull <33642481+Skull220@users.noreply.github.com>
2020-05-19 16:51:29 -0400

    renamed some variables

--- basicOperations.cpp
+++ basicOperations.cpp
@@ -1,51 +1,49 @@
 #include "header.h"
 
-//TODO: Since I wrote these out myself, I need to find a way to handle negatives. 
-//Decimals I'm just gonna have the computer handle since I have no idea how I could write that myself.
+/*TODO: Since I wrote these out myself, I need to find a way to handle negatives. 
+Decimals I'm just gonna have the computer handle since I have no idea how I could write that myself. */
 
-//wrote these out manually as well, just to be a joker
-int addition(int a, int b){
-    int result = a;
-    for (int i = 0; i < b; i++){
+//wrote these out manually as well
+int addition(int first_term, int second_term){
+    int result = first_term;
+    for (int i = 0; i < second_term; i++){
         result++;
     }
-
     return result;
 }
 
-int subtraction(int a, int b){
-    int result = a;
-    for (int i = 0; i < b; i++){
+int subtraction(int first_term, int second_term){
+    int result = first_term;
+    for (int i = 0; i < second_term; i++){
         result--;
     }
-
     return result;
 }
 
-int multiplication(int a, int b){
+int multiplication(int first_factor, int second_factor){
     int result = 0;
-    for (int i = 0; i < b; i++){
-        result = addition(result, a);
+    for (int i = 0; i < second_factor; i++){
+        result = addition(result, first_factor);
     }
     return result;
 }
 
 //this is a pain in the ass and is making me regret my decision above
-double division(double a, double b){
+double division(double dividend, double divisor){
     double result = 0;
-    while(a > b-1){
-        a -= b;
+    while(dividend > divisor-1){
+        dividend -= divisor;
         result++;
     } 
-    if ((a - b) != 0){
+    if ((dividend - divisor) != 0){
         //I can't currently think of a way to do this without the division operator.
-        result = result + (a/b);
+        result = result + (dividend/divisor);
     }
     return result;
 }
 
-void comparator(int a, int b){
-    if(a == b){
+void comparator(int left_side, int right_side){
+    if(left_side == right_side){
         cout << "True!" << "\n";
     } else {
         cout << "False." << "\n";
