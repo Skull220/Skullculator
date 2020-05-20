927690e0f04c657f287adc3166c27d422e81f78a
Skull <33642481+Skull220@users.noreply.github.com>
2020-05-20 14:56:42 -0400

    Updated with new functions and class.

--- header.h
+++ header.h
@@ -1,15 +1,29 @@
 #include <iostream>             
 #include <cmath>
 #include <string>
+#include <climits>
 
 using namespace std;
 
-int addition(int a, int b);
-int subtraction(int a, int b);
-int multiplication(int a, int b);
-double division(double a, double b);
-int exponent(int a, int b);
-void comparator(int a, int b);
+class history{
+    private:
+        string history_list[20];
+        int history_slot;
+    public:
+        void clear_history();
+        void commit_to_history(string input);
+        double display_history();
+        void help_menu();
+};
+
+int addition(int first_term, int second_term);
+int subtraction(int first_term, int second_term);
+int multiplication(int first_factor, int second_factor);
+double division(double dividend, double divisor);
+void comparator(double left_side, double right_side);
+double exponent(int base, int exponent);
+double take_root(double index, double radicand);
+
 
 
 
