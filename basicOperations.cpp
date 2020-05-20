106005a0327005fa3e835486b008eba6161f8edc
Skull <33642481+Skull220@users.noreply.github.com>
2020-05-20 14:54:43 -0400

    Cleanup

--- basicOperations.cpp
+++ basicOperations.cpp
@@ -1,30 +1,24 @@
 #include "header.h"
 
-/*TODO: Since I wrote these out myself, I need to find a way to handle negatives. 
+/* TODO: Since I wrote these out myself, I need to find a way to handle negatives. 
 Decimals I'm just gonna have the computer handle since I have no idea how I could write that myself. */
 
 //wrote these out manually as well
 int addition(int first_term, int second_term){
     int result = first_term;
-    for (int i = 0; i < second_term; i++){
-        result++;
-    }
+    for (int i = 0; i < second_term; i++) result++;
     return result;
 }
 
 int subtraction(int first_term, int second_term){
     int result = first_term;
-    for (int i = 0; i < second_term; i++){
-        result--;
-    }
+    for (int i = 0; i < second_term; i++) result--;
     return result;
 }
 
 int multiplication(int first_factor, int second_factor){
     int result = 0;
-    for (int i = 0; i < second_factor; i++){
-        result = addition(result, first_factor);
-    }
+    for (int i = 0; i < second_factor; i++) result = addition(result, first_factor);
     return result;
 }
 
@@ -35,17 +29,13 @@ double division(double dividend, double divisor){
         dividend -= divisor;
         result++;
     } 
-    if ((dividend - divisor) != 0){
-        //I can't currently think of a way to do this without the division operator.
-        result = result + (dividend/divisor);
-    }
+    //I can't currently think of a way to do this without the division operator.
+    if ((dividend - divisor) != 0) result = result + (dividend/divisor);
     return result;
 }
 
-void comparator(int left_side, int right_side){
-    if(left_side == right_side){
-        cout << "True!" << "\n";
-    } else {
-        cout << "False." << "\n";
-    }
+//easy as shit and will work for doubles
+void comparator(double left_side, double right_side){
+    if(left_side == right_side) cout << "True!" << "\n";
+    else cout << "False." << "\n";
 }
