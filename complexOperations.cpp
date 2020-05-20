126a7aa3d1b4d8fb2bbcc4638feb36308b73eeb4
Skull <33642481+Skull220@users.noreply.github.com>
2020-05-20 14:56:29 -0400

    Cleaned up the exponent function and added a root function

--- complexOperations.cpp
+++ complexOperations.cpp
@@ -3,10 +3,14 @@
 //TODO: Since I wrote these out myself, I need to find a way to handle negatives.
 
 //calculates, prints, and returns the result. I could use a built in function but I want to do this myself
-int exponent(int base, int exponent){
+double exponent(int base, int exponent){
     double result = 1;
-    if (exponent != 0){ 
-        for(int i = 0; i < exponent; i++) result *= base;
-    }
+    if (exponent != 0) for(int i = 0; i < exponent; i++) result *= base;
+    return result;
+}
+/* If I ever decide one day that I am a failure of a human being and I deserve nothing but endless suffering,
+I'll write this process manually. For now I'm just going to use a built in function.*/
+double take_root(double index, double radicand){
+    double result = pow(radicand, (1/index));
     return result;
 }
