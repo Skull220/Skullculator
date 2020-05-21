33e2757d3f955e876180b8f1c534ec6cbc9bf0a8
Skull <33642481+Skull220@users.noreply.github.com>
2020-05-21 19:18:07 -0400

    Added negative support to exponent and an imaginary checker to take_root.

--- complexOperations.cpp
+++ complexOperations.cpp
@@ -3,13 +3,22 @@
 //calculates, prints, and returns the result. I could use a built in function but I want to do this myself
 double exponent(int base, int exponent){
     double result = 1;
-    if (exponent != 0) for(int i = 0; i < exponent; i++) result *= base;
+    if (exponent == 0) return result;
+    for(int i = 0; i < abs(exponent); i++) result = multiplication(result, base);
+    if (exponent < 0){
+        return (1/result);
+    }
     return result;
 }
 
-/* If I ever decide one day that I am a failure of a human being and I deserve nothing but endless suffering,
-I'll write this process manually. For now I'm just going to use a built in function.*/
+/* For now I'm just going to use a built in function.
+If I ever decide one day that I am a failure of a human being and I deserve nothing but endless suffering,
+I'll write this process manually. .*/
+
 double take_root(double index, double radicand){
+    if (radicand < 0){
+        return -1;
+    }
     double result = pow(radicand, (1/index));
     return result;
 }
